23c57e343dca6a6172ecd218f8ec03b81bd1d548
Fractal-Tess <vgfractal@gmail.com>
2023-01-11 13:00:03 +0200

    added gpu

---
+++ themes/Fractal-Tess/scripts/gpu_load.sh
@@ -0,0 +1,1 @@
+nvidia-smi -q -d utilization | rg -i gpu | sed -n '3p' | awk '{print $3}'
---
+++ themes/Fractal-Tess/scripts/gpu_temp.sh
@@ -0,0 +1,1 @@
+nvidia-smi -q -d temperature | rg -i "gpu current" | awk -F ":" '{print $2}' | sed 's/c\| //ig'
